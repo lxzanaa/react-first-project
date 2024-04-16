6164f567664f88cdb6a53ac8e7a600ffcc48986a
lxzanaa <zari.weone2010@gmail.com>
2024-04-16 23:19:55 +0300

    testimonial section ended

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Home from "./components/Home"
 import About from "./components/About"
+import Work from "./components/Work"
+import Testimonial from "./components/Testimonial"
 
 function App() {
   return (
     <div>
       <Home />
       <About />
+      <Work/>
+      <Testimonial/>
     </div>
 
   )
