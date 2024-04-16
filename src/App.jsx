086bf36cfc127d420bc3a9c6a074edc45235339c
lxzanaa <zari.weone2010@gmail.com>
2024-04-16 21:49:12 +0300

    header end

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div>
+      <Home/>  <Navbar/>
+    </div>
+   
+  )
+}
+
+export default App
